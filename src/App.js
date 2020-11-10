14879cb1490554cd1d7283a8bb63fad7150d181e
MohamedKerkeb <kerkeb.moha@gmail.com>
2020-11-10 21:50:45 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,89 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+
+import Axios from 'axios';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faRedoAlt } from '@fortawesome/free-solid-svg-icons';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	const [quote, setQuote] = useState([]);
+	const [quotesAuthor, setQuotesAuthor] = useState([]);
+	const [message, setMessage] = useState('');
+	const [vue, setVue] = useState('false');
+
+	const fetchData = async () => {
+		const getQuote = await Axios.get('https://quote-garden.herokuapp.com/api/v2/quotes/random');
+		setQuote([
+			{
+				text: getQuote.data.quote.quoteText,
+				author: getQuote.data.quote.quoteAuthor,
+				genre: getQuote.data.quote.quoteGenre,
+				id: getQuote.data.quote._id,
+			},
+		]);
+		setVue(true);
+	};
+
+	const fetchAuthor = async (author) => {
+		const getAuthorQuotes = await Axios.get(
+			`https://quote-garden.herokuapp.com/api/v2/authors/${author}?page=1&limit=10`
+		);
+		const quoteText = getAuthorQuotes.data.quotes.map((q) => q.quoteText);
+		const message = getAuthorQuotes.data.message;
+
+		setQuotesAuthor(quoteText);
+		setMessage(message);
+		setVue(false);
+	};
+
+	// console.log(quotesAuthor);
+
+	useEffect(() => {
+		fetchData();
+	}, []);
+
+	return (
+		<>
+			<div className='main'>
+				<header>
+					<button onClick={() => fetchData()}>
+						<FontAwesomeIcon icon={faRedoAlt} /> Ramdom
+					</button>
+				</header>
+				<section className='wrapperQuote'>
+					{vue ? (
+						quote.map((q) => (
+							<div key={q.id}>
+								<p className='quote'>{q.text}</p>
+								<div className='wrapperAuthor'>
+									<h4 className='author' onClick={() => fetchAuthor(q.author)}>
+										{q.author}
+									</h4>
+									<span className='genre'>{q.genre}</span>
+								</div>
+							</div>
+						))
+					) : (
+						<div>
+							<h2>{message}</h2>
+							{quotesAuthor.map((t, i) => (
+								<div>
+									<p className='quote' key={i}>
+										{t}
+									</p>
+								</div>
+							))}
+						</div>
+					)}
+				</section>
+				<footer>
+					<p>Mohamed@devchallanges.io</p>
+				</footer>
+			</div>
+		</>
+	);
+};
 
 export default App;
